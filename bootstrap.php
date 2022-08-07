8a9e5faa8363d90e6c9dcf7e2c50b78fbfc83f0f
sara0918 <108059277+sara0918@users.noreply.github.com>
2022-08-07 10:11:57 +0300

    Create bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+define("PROJECT_ROOT_PATH", __DIR__ . "/../");
+ 
+require_once PROJECT_ROOT_PATH . "/inc/config.php";
+ 
+require_once PROJECT_ROOT_PATH . "/Controller/Api/BaseController.php";
+
+require_once PROJECT_ROOT_PATH . "/Model/NumberModel.php";
+?>
+Footer
